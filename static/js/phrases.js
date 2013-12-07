ad01f1b9ee453eaa922f87402ff45336937bb499
John Mumm <jtfmumm@gmail.com>
2013-12-07 17:13:43 -0500

    Setup scheduler

--- static/js/phrases.js
+++ static/js/phrases.js
@@ -1,14 +1,22 @@
+var settings = null;
 
 
-var majorScale = [0, 2, 4, 5, 7, 9, 11, 12];
+// var settings = {
+// 	tempo: 120,
+// 	scale: "majorScale"
+//  sections: [
+		// [[0, 30], [4, 50], [14, 40], [13, 20]...], [phraseIndex, rootValue] pairs 
+		// ...
+		// ]
+// }
 
-var settings = {
-	tempo: 120,
-	scale: majorScale
+var scales = {
+	"majorScale": [0, 2, 4, 5, 7, 9, 11, 12]
 }
 
 var getNoteNumber = function(scaleDegree, rootValue) {
-	var noteNumber;
+	var noteNumber, newDegree = null;
+	var curScale = scales[settings.scale];
 
 	//Check for invalid input
 	if (scaleDegree === 0 || scaleDegree === -1) {
@@ -16,11 +24,11 @@ var getNoteNumber = function(scaleDegree, rootValue) {
 	} 
 
 	if (scaleDegree > 0) {
-		var newDegree = scaleDegree - 1;
-		noteNumber = rootValue + settings.scale[newDegree];
+		newDegree = scaleDegree - 1;
+		noteNumber = rootValue + curScale[newDegree];
 	} else {
-		var newDegree = settings.scale.length + (scaleDegree - 1);
-		noteNumber = (rootValue - 12) + settings.scale[newDegree];
+		newDegree = curScale.length + (scaleDegree - 1);
+		noteNumber = (rootValue - 12) + curScale[newDegree];
 	}
 	return noteNumber;
 }
@@ -53,18 +61,28 @@ var playOsc = function(freq, dur, startTime) {
 	osc.stop(startTime + dur);
 }
 
-var playPhrase = function(phraseStartTime, phraseRoot, phrase) {
-	console.log("playPhrase");
-	var startTime = phraseStartTime;
-	for (var i = 0; i < phrase.notes.length; i++) {
-		var beatValue = (1 / (settings.tempo / 60)) * 4; 
-		var toneLookup = getNoteNumber(phrase.notes[i].tone, phraseRoot);
-		var freq = _tonesTable[toneLookup];
-    	var duration = beatValue / phrase.notes[i].noteValue;
-    	playOsc(freq, duration, startTime);
-		var startTime = startTime + duration;
-	}
-}
+//Representing relative notes as scale degrees 1-7
+var phrases = [
+	makePhrase([[1, 2]]),
+	makePhrase([[5, 4], [1, 4]]),
+	makePhrase([[1, 8], [5, 8], [8, 8], [5, 8]]),
+	makePhrase([[1, 8], [3, 8], [5, 8], [3, 8]]),
+	makePhrase([[1, 8], [2, 8], [3, 8], [5, 8]]),
+	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]]),
+	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]]),
+	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]]),
+	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]]),
+	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]]),
+	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]]),
+	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]]),
+	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]]),
+	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]]),
+	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]]),
+	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]])
+	//makePhrase([[1, 8], [3, 8], [5, 8], [3, 8]]),
+	//makePhrase([[1, 8], [3, 8], [5, 8], [3, 8]]),
+	//makePhrase([[1, 8], [3, 8], [5, 8], [3, 8]])
+]
 
 function Note(tone, noteValue) {
 	this.tone = tone;
@@ -84,37 +102,60 @@ function makePhrase(notes) {
 	return new Phrase(tempPhrase);
 }
 
+var playPhrase = function(section, phrase) {
+	console.log("playPhrase");
+	var startTime = ctx.currentTime;
+	var phraseRoot = settings["sections"][section][phrase][1];
+	phrase = settings["sections"][section][phrase][0];
+	for (var i = 0; i < phrase.notes.length; i++) {
+		var beatValue = (1 / (settings.tempo / 60)) * 4; 
+		var toneLookup = getNoteNumber(phrase.notes[i].tone, phraseRoot);
+		var freq = _tonesTable[toneLookup];
+    	var duration = beatValue / phrase.notes[i].noteValue;
+    	playOsc(freq, duration, startTime);
+		var startTime = startTime + duration;
+	}
+}
+
+var getDuration = function() {
+	var beatValue = var beatValue = (1 / (settings.tempo / 60));
+	var duration = (2 * beatValue) * 1000; //Convert to ms for setTimeout
+	return duration; 
+}
+
+var getNextPhrase = function(section, phrase) {
+	if (++phrase > 7) {
+		section = (section + 1) % 8;
+		phrase = 0;
+	} 
+	return { 
+		"section": section,
+		"phrase": phrase 
+	}
+}
+
+var scheduler = function(section, phrase) {
+	var duration = getDuration();
+	var nextPhraseObj = getNextPhrase(section, phrase);
+	playPhrase(phrase);
+	setTimeout(function() { 
+		scheduler(nextPhraseObj["section"], nextPhraseObj["phrase"]); 
+	}, duration);
+}
+
+//playPhrase schedules next phrase
 
 
-//Representing relative notes as scale degrees 1-7
-var phrases = [
-	makePhrase([[1, 2]]),
-	makePhrase([[5, 4], [1, 4]]),
-	makePhrase([[1, 8], [5, 8], [8, 8], [5, 8]]),
-	makePhrase([[1, 8], [3, 8], [5, 8], [3, 8]]),
-	makePhrase([[1, 8], [2, 8], [3, 8], [5, 8]]),
-	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]])
+//playPhrase(0, 15, aPhrase);
 
-	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]])
-	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]])
+//playPhrase(0, 15, phrases[0]);
+//playPhrases(phrases);
 
-	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]])
-	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]])
-	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]])
-	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]])
-	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]])
-	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]])
-	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]])
-	makePhrase([[1, 8], [3, 16], [-4, 16], [5, 16], [-3, 16], [6, 16], [-2, 16]])
-	//makePhrase([[1, 8], [3, 8], [5, 8], [3, 8]]),
-	//makePhrase([[1, 8], [3, 8], [5, 8], [3, 8]]),
-	//makePhrase([[1, 8], [3, 8], [5, 8], [3, 8]])
-]
+socket.on("state.current", function(data) {
+    settings = data; }
+	);
 
-
-var theNotes = [new Note(0, 8), new Note(5, 8), new Note(8, 8), new Note(-4, 8)];
-
-var aPhrase = new Phrase(theNotes);
+scheduler(0, 0);
 
 var playPhrases = function(phrases) {
 	var startTime = ctx.currentTime;
@@ -124,11 +165,3 @@ var playPhrases = function(phrases) {
 		startTime = startTime + (beatValue / 2);
 	}
 }
-
-//playPhrase(0, 15, aPhrase);
-
-//playPhrase(0, 15, phrases[0]);
-playPhrases(phrases);
-
-
-
